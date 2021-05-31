a58c768b0c3f4b378720a96e16253cc425dfe555
传说故事 <1114976722@qq.com>
2021-05-31 21:34:54 +0800

    more stable

--- src/config.py
+++ src/config.py
@@ -1,3 +1,7 @@
 test_size = 0.2  # 测试集所占比例
-lim = (0.01, 10)  # 最小信息增益与最小样本数
+lim = (0.01, 5)  # 最小信息增益与最小样本数=1,5,10
 rand = 1  # 随机数种子
+assert test_size == 0.2
+assert lim[0] == 0.01
+assert rand == 1
+assert lim[1] == 1 or 5 or 10
